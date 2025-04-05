4f7b421753395dceb66c14838831fc8e5f1658d5
RaulSeguraHernandez <raul3ro.segura@gmail.com>
2025-04-04 23:49:57 -0600

    cambio en un test

--- src/ArrayCircularQueue.c
+++ src/ArrayCircularQueue.c
@@ -1,5 +1,5 @@
 #define LINKED_LIST_IMPLEMENTATION
-#include "..\include\linked_list_DEQueue.h"
+#include "..\include\linked_list_Circular_Queue.h"
 
 void print_int(int value) {
     printf("%d", value);
@@ -12,21 +12,21 @@ void print_float(float value) {
 }
 
 int main(){
-    List_int* Lista = DEQueue_int_create();
-    DEQueue_int_enqueue_head(Lista , 1);
-    DEQueue_int_enqueue_head(Lista , 2);
-    DEQueue_int_enqueue_tail(Lista , 3);
-    DEQueue_int_enqueue_tail(Lista , 4);
-    printf("Primero %d\n", DEQueue_int_first(Lista));
-    printf("Ultimo %d\n", DEQueue_int_last(Lista));
-    DEQueue_int_print(Lista, print_int);
-    DEQueue_int_dequeue_tail(Lista);
-    printf("Primero %d\n", DEQueue_int_first(Lista));
-    printf("Ultimo %d\n", DEQueue_int_last(Lista));
-    DEQueue_int_print(Lista, print_int);
-    DEQueue_int_dequeue_head(Lista);
-    DEQueue_int_dequeue_head(Lista);
-    DEQueue_int_dequeue_tail(Lista);
-    printf("Vacia %i\n", DEQueue_int_is_empty(Lista));
+    List_int* Lista = CircularQueue_int_create();
+    CircularQueue_int_enqueue(Lista , 1);
+    CircularQueue_int_enqueue(Lista , 2);
+    CircularQueue_int_enqueue(Lista , 3);
+    CircularQueue_int_enqueue(Lista , 4);
+    printf("Primero %d\n", CircularQueue_int_first(Lista));
+    printf("Ultimo %d\n", CircularQueue_int_last(Lista));
+    CircularQueue_int_print(Lista, print_int);
+    CircularQueue_int_dequeue(Lista);
+    printf("Primero %d\n", CircularQueue_int_first(Lista));
+    printf("Ultimo %d\n", CircularQueue_int_last(Lista));
+    CircularQueue_int_print(Lista, print_int);
+    CircularQueue_int_dequeue(Lista);
+    CircularQueue_int_dequeue(Lista);
+    CircularQueue_int_dequeue(Lista);
+    printf("Vacia %i\n", CircularQueue_int_is_empty(Lista));
     return 0;
 }
